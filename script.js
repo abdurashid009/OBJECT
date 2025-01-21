b08fbea08613fb396c7c6179de6063bc8c17c3c8
Abdurashid <abdurashidnormetov2009.26@gmail.com>
2025-01-21 22:45:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let admin = prompt("Ismingizni Kiriting")
+let admin2 = prompt("ID Kiriting")
+let admin3 = prompt("Passwordingizni Kiriting")
+
+let admin4 = {
+    name:`${admin}`,
+    id:`${admin2}`,
+    password:`${admin3}`,
+}
+console.log(admin4)
